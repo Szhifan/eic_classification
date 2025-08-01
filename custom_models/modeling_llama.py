8348c73d30e201235d78d8afe1e829cb6eed9792
Szhifan <sunzhifan233@gmail.com>
2025-08-01 12:32:33 +0200

    fix

--- custom_models/modeling_llama.py
+++ custom_models/modeling_llama.py
@@ -1,23 +1,17 @@
 import logging
 from typing import List, Optional, Tuple, Union
-from functools import partial
 
 import torch
 from torch import nn
 
-from transformers.cache_utils import Cache, DynamicCache, StaticCache
+from transformers.cache_utils import Cache
 from transformers.modeling_outputs import BaseModelOutputWithPast, SequenceClassifierOutput
 from transformers.models.llama.modeling_llama import (
     LlamaPreTrainedModel, 
     LlamaDecoderLayer, 
     LlamaRMSNorm, 
     LlamaRotaryEmbedding,
-    _prepare_4d_causal_attention_mask_with_cache_position,
 )
-from transformers.modeling_attn_mask_utils import AttentionMaskConverter
-from transformers.utils import TransformersKwargs, logging
-from transformers.processing_utils import Unpack
-from transformers.modeling_flash_attention_utils import FlashAttentionKwargs
 from .modeling_utils import (
     get_noncausal_attention_mask,
     get_noncausal_attention_mask_0,
@@ -25,7 +19,13 @@ from .modeling_utils import (
     use_res_connect,
     flip_tensor,
 )
+from functools import partial
+from transformers.utils import TransformersKwargs, logging
+from transformers.processing_utils import Unpack
+from transformers.cache_utils import DynamicCache
+from transformers.masking_utils import create_causal_mask
 
+logger = logging.getLogger(__name__)
 
 class LlamaModel(LlamaPreTrainedModel):
     def __init__(self, config):
@@ -92,11 +92,9 @@ class LlamaModel(LlamaPreTrainedModel):
         position_ids: Optional[torch.LongTensor] = None,
         past_key_values: Optional[Cache] = None,
         inputs_embeds: Optional[torch.FloatTensor] = None,
-        use_cache: Optional[bool] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
         cache_position: Optional[torch.LongTensor] = None,
-        **flash_attn_kwargs: Unpack[FlashAttentionKwargs],
+        use_cache: Optional[bool] = None,
+        **kwargs: Unpack[TransformersKwargs],
     ) -> Union[Tuple, BaseModelOutputWithPast]:
         if (input_ids is None) ^ (inputs_embeds is not None):
             raise ValueError("You must specify exactly one of input_ids or inputs_embeds")
@@ -115,11 +113,19 @@ class LlamaModel(LlamaPreTrainedModel):
 
         if position_ids is None:
             position_ids = cache_position.unsqueeze(0)
+        return_legacy_cache = False
         _is_flash_attn = self.config._attn_implementation == "flash_attention_2"
         _is_intera = self.architecture in {'INTER', 'EXTRA'}
         _is_mask0 = self.mask_type == "MASK0"
 
-        causal_mask = self._update_causal_mask(attention_mask, inputs_embeds, cache_position, past_key_values, output_attentions)
+        causal_mask = create_causal_mask(
+            config=self.config,
+            input_embeds=inputs_embeds,
+            attention_mask=attention_mask,
+            cache_position=cache_position,
+            past_key_values=past_key_values,
+            position_ids=position_ids,
+        )
 
         # Get input shape from either input_ids or inputs_embeds
         input_shape = input_ids.shape if input_ids is not None else inputs_embeds.shape[:2]
@@ -161,35 +167,19 @@ class LlamaModel(LlamaPreTrainedModel):
 
             hidden_states = flip_tensor(hidden_states, reverse_flag)
 
-            if self.gradient_checkpointing and self.training:
-                layer_outputs = self._gradient_checkpointing_func(
-                    partial(decoder_layer.__call__, **flash_attn_kwargs),
-                    hidden_states,
-                    causal_mask,
-                    position_ids,
-                    past_key_values,
-                    output_attentions,
-                    use_cache,
-                    cache_position,
-                    position_embeddings,
-                )
-            else:
-                layer_outputs = decoder_layer(
-                    hidden_states,
-                    attention_mask=layer_mask,
-                    position_ids=position_ids,
-                    past_key_value=past_key_values,
-                    output_attentions=output_attentions,
-                    use_cache=use_cache,
-                    cache_position=cache_position,
-                    position_embeddings=position_embeddings,
-                    **flash_attn_kwargs,
-                )
 
-            hidden_states = flip_tensor(layer_outputs[0], reverse_flag)
+            hidden_states = decoder_layer(
+                hidden_states,
+                attention_mask=layer_mask,
+                position_ids=position_ids,
+                past_key_value=past_key_values,
+                cache_position=cache_position,
+                position_embeddings=position_embeddings,
+            )
 
-            if output_attentions:
-                all_self_attns += (layer_outputs[1],)
+            hidden_states = flip_tensor(hidden_states, reverse_flag)
+
+
         forward_hidden_states = h1 if _is_intera else h2
         for i in range(self.bidir_layers, self.config.num_hidden_layers if _is_intera else 0):
             decoder_layer = self.layers[i]
@@ -212,10 +202,11 @@ class LlamaModel(LlamaPreTrainedModel):
         hidden_states = self.norm(hidden_states)
         # add hidden states from the last decoder layer
 
-        if output_hidden_states:
-            all_hidden_states += (hidden_states,)
+        all_hidden_states += (hidden_states,)
 
         next_cache = next_decoder_cache if use_cache else None
+        if return_legacy_cache:
+            next_cache = next_cache.to_legacy_cache()
 
         return BaseModelOutputWithPast(
             last_hidden_state=hidden_states,
@@ -243,72 +234,8 @@ class LlamaModel(LlamaPreTrainedModel):
         first_new_layer = self.config.num_hidden_layers
         for layer in range(first_new_layer, len(self.layers)):
             self.layers[layer].load_state_dict(self.layers[first_new_layer - 1].state_dict())
-    def _update_causal_mask(
-        self,
-        attention_mask: torch.Tensor,
-        input_tensor: torch.Tensor,
-        cache_position: torch.Tensor,
-        past_key_values: Cache,
-        output_attentions: bool,
-    ):
-        if self.config._attn_implementation == "flash_attention_2":
-            if attention_mask is not None and 0.0 in attention_mask:
-                return attention_mask
-            return None
 
-        # For SDPA, when possible, we will rely on its `is_causal` argument instead of its `attn_mask` argument, in
-        # order to dispatch on Flash Attention 2. This feature is not compatible with static cache, as SDPA will fail
-        # to infer the attention mask.
-        past_seen_tokens = past_key_values.get_seq_length() if past_key_values is not None else 0
-        using_static_cache = isinstance(past_key_values, StaticCache)
 
-        # When output attentions is True, sdpa implementation's forward method calls the eager implementation's forward
-        if self.config._attn_implementation == "sdpa" and not using_static_cache and not output_attentions:
-            if AttentionMaskConverter._ignore_causal_mask_sdpa(
-                attention_mask,
-                inputs_embeds=input_tensor,
-                past_key_values_length=past_seen_tokens,
-                is_training=self.training,
-            ):
-                return None
-
-        dtype, device = input_tensor.dtype, input_tensor.device
-        min_dtype = torch.finfo(dtype).min
-        sequence_length = input_tensor.shape[1]
-        if using_static_cache:
-            target_length = past_key_values.get_max_cache_shape()
-        else:
-            target_length = (
-                attention_mask.shape[-1]
-                if isinstance(attention_mask, torch.Tensor)
-                else past_seen_tokens + sequence_length + 1
-            )
-
-        # In case the provided `attention` mask is 2D, we generate a causal mask here (4D).
-        causal_mask = _prepare_4d_causal_attention_mask_with_cache_position(
-            attention_mask,
-            sequence_length=sequence_length,
-            target_length=target_length,
-            dtype=dtype,
-            device=device,
-            min_dtype=min_dtype,
-            cache_position=cache_position,
-            batch_size=input_tensor.shape[0],
-        )
-
-        if (
-            self.config._attn_implementation == "sdpa"
-            and attention_mask is not None
-            and attention_mask.device.type == "cuda"
-            and not output_attentions
-        ):
-            # Attend to all tokens in fully masked rows in the causal_mask, for example the relevant first rows when
-            # using left padding. This is required by F.scaled_dot_product_attention memory-efficient attention path.
-            # Details: https://github.com/pytorch/pytorch/issues/110213
-            min_dtype = torch.finfo(dtype).min
-            causal_mask = AttentionMaskConverter._unmask_unattended(causal_mask, min_dtype)
-
-        return causal_mask
 class LlamaForSequenceClassification(LlamaPreTrainedModel):
     """
     Simple wrapper that adds a classification head to the custom LlamaModel
